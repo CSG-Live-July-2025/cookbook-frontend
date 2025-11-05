85387e3d78a98d645bc23ed6e1ce658ed04adb29
leonshimizu <lmshimizu@gmail.com>
2025-11-05 17:31:29 +1000

    Comment out extra styling

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -2,18 +2,36 @@ export function RecipesIndex(props) {
   return (
     <div id="recipes-index">
       <h1>All {props.recipes_prop.length} Recipes</h1>
-      <div className="cards">
-        {props.recipes_prop.map((recipe) => (
-          <div key={recipe.id} className="card">
-            <img src={recipe.image_url} />
-            <div class="card-body">
-              <h2>{recipe.title}</h2>
-              <p>Chef: {recipe.chef}</p>
-              <button>More Info</button>
-            </div>
-          </div>
-        ))}
-      </div>
+      {props.recipes_prop.map((recipe) => (
+        <div key={recipe.id} className="card">
+          <img src={recipe.image_url} />
+          <h2>{recipe.title}</h2>
+          <p>Chef: {recipe.chef}</p>
+          <button>More Info</button>
+        </div>
+      ))}
     </div>
   );
 }
+
+
+// with extra styling:
+// export function RecipesIndex(props) {
+//   return (
+//     <div id="recipes-index">
+//       <h1>All {props.recipes_prop.length} Recipes</h1>
+//       <div className="cards">
+//         {props.recipes_prop.map((recipe) => (
+//           <div key={recipe.id} className="card">
+//             <img src={recipe.image_url} />
+//             <div class="card-body">
+//               <h2>{recipe.title}</h2>
+//               <p>Chef: {recipe.chef}</p>
+//               <button>More Info</button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
